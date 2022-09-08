8265031262eef6b66a50793e82923bb556cf5176
Mohammed Ibrahim <97616813+Bryhim4code@users.noreply.github.com>
2022-09-08 04:39:42 -0700

    Add files via upload
    my apple clone web page

---
+++ Inc/javascript/index.js
@@ -0,0 +1,9 @@
+let searchBar = document.getElementById("searchBar");
+
+function openSearch(){
+    searchBar.classList.add("show")
+}
+
+function closeSearch(){
+    searchBar.classList.remove("show")
+}
